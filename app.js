3021f4065ff50180ea449076bf8bb86e7bf80cb9
unknown <michaelabebe19922000@gmail.com>
2022-09-01 14:35:09 +0300

    Heroku

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const request = require("request")
-const ejs = require("ejs")
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const ejs = require("ejs");
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"))
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-
-app.get("/", function(req, res) {
-res.render("home")
-})
-app.get("/about", function(req, res) {
-    res.render("about")
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
- app.get("/fantu", function(req, res) {
-    res.render("fantu")
-  })
+);
 
-app.get("/contact", function(req, res) {
-    res.render("contact")
-  })
-app.get("/home", function(req, res) {
-    res.render("home")
-  })
-app.get("/Tourism", function(req, res) {
-  res.render("Tourism")
-})
-app.get("/Governance", function(req, res) {
-  res.render("Governance")
-})
-app.get("/Risk", function(req, res) {
-  res.render("Risk")
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+app.get("/fantu", function (req, res) {
+  res.render("fantu");
+});
 
-app.listen(3000, function() {
- console.log("Server is live on port 3000")
-})
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+app.get("/home", function (req, res) {
+  res.render("home");
+});
+app.get("/Tourism", function (req, res) {
+  res.render("Tourism");
+});
+app.get("/Governance", function (req, res) {
+  res.render("Governance");
+});
+app.get("/Risk", function (req, res) {
+  res.render("Risk");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is live on port 3000");
+});
